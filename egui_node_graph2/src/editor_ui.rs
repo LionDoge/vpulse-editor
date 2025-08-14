31d58a92d23e335f713ec5965bba92b3323c7f9a
Lion Doge <KrystianoXPL@gmail.com>
2025-08-14 22:22:46 +0200

    marginally improve zooming

--- egui_node_graph2/src/editor_ui.rs
+++ egui_node_graph2/src/editor_ui.rs
@@ -778,7 +778,7 @@ where
                         self.graph.connections(param_id).len(),
                         max_connections,
                     );
-                    let margin = 5.0;
+                    let margin = 5.0 * pan_zoom.zoom;
                     let missing_space =
                         port_height - (height_intermediate - height_before) + margin;
                     if missing_space > 0.0 {
